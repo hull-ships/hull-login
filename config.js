d2fb7ad4680e682bbaf63daa9cea7388675b1c90
Romain Dardour <romain@hull.io>
2015-03-31 14:53:58 +0200

    move pictures

--- config.js
+++ config.js
@@ -37,6 +37,7 @@ var files = {
   "src/vendors/**/*" : gulpDest("vendors/"),
   "src/images/**/*"  : gulpDest("images/"),
   "manifest.json"    : outputFolder,
+  "src/*.png"        : outputFolder,
   "src/*.html"       : outputFolder,
   "CNAME"            : outputFolder,
 };
