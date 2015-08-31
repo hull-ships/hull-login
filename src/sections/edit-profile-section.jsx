4094bb90ebae3b5ff95c83eddf47b44f66d01cc1
Stephane Bellity <sbellity@gmail.com>
2015-08-31 19:18:36 +0200

    ...

--- src/sections/edit-profile-section.jsx
+++ src/sections/edit-profile-section.jsx
@@ -1,36 +1,30 @@
 import React from 'react';
-import { bind } from 'underscore';
 import { TranslatedMessage, UserImage } from '../components';
 import { EditProfileForm } from '../forms';
 import BaseSection from './base-section';
 
 export default class SignUpSection extends BaseSection {
 
-  handleSubtitleClick(e) {
-    if (e && e.preventDefault) e.preventDefault();
-    if (this.props.formIsSubmitted || !this.props.hasForm) {
-      this.props.activateShowProfileSection();
-    } else {
-      this.props.hideDialog();
-    }
-  }
-
   renderHeader(styles) {
     let title;
     let subtitle;
+    let handleClick;
+
     if (this.props.formIsSubmitted || !this.props.hasForm) {
       title = 'edit profile header';
       subtitle = 'edit profile cancel button';
+      handleClick = this.props.activateShowProfileSection.bind(this);
     } else {
       title = 'edit profile header when profile incomplete';
       subtitle = 'edit profile cancel button when profile incomplete';
+      handleClick = this.props.hideDialog.bind(this);
     }
 
     let { picture } = this.props.user || {};
     return <div style={styles.sectionHeader}>
       <UserImage style={styles.sectionUserImage} src={picture} />
       <TranslatedMessage tag='h1' style={styles.sectionTitle} message={title} />
-      <a href='javascript: void 0;' onClick={bind(this.handleSubtitleClick, this)}>
+      <a href='javascript: void 0;' onClick={handleClick}>
         <TranslatedMessage tag='p' style={styles.sectionText} message={subtitle} />
       </a>
     </div>;
