ecd76b5d61eb95fec75a3072af8dda0074339e64
Stephane Bellity <sbellity@gmail.com>
2015-09-01 18:14:47 +0200

    Help style

--- src/components/help/index.jsx
+++ src/components/help/index.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import { StyleResolverMixin, BrowserStateMixin } from 'radium';
 
+const styles = {
+  display: 'block',
+  textAlign: 'center',
+  fontSize: 12,
+  marginTop: 0,
+  marginBottom: 10,
+  opacity: 0.5
+};
+
 export default React.createClass({
   displayName: 'Help',
 
@@ -10,8 +19,8 @@ export default React.createClass({
   ],
 
   render() {
-    return <span style={{display: 'block', 'textAlign': 'center', 'marginBottom': '10px'}}>
-        {this.props.children}
+    return <span style={styles}>
+      {this.props.children}
     </span>;
   }
 });
