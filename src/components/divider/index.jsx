134c609389b7c8205ec77d0bc91732f9b20e8a94
Jimmy Oliger <hi@jimmyoliger.com>
2015-09-07 12:35:07 +0200

    center legend in fieldset in firefox
    Had to set the attribute on the DOM element manualy because React strip the
    align attribute...

--- src/components/divider/index.jsx
+++ src/components/divider/index.jsx
@@ -4,14 +4,19 @@ import { getStyles } from './styles';
 export default React.createClass({
   displayName: 'Divider',
 
+  componentDidMount() {
+    // React does not support the `align` attribute. Firefox needs it to center
+    // the `<legend />` inside the `<fieldset />`.
+    this.refs.content.getDOMNode().setAttribute('align', 'center');
+  },
+
   render() {
     const styles = getStyles();
 
     return (
       <fieldset style={styles.divider}>
-        <legend align='center' style={styles.dividerContent}>{this.props.children}</legend>
+        <legend ref='content' style={styles.dividerContent}>{this.props.children}</legend>
       </fieldset>
     );
   }
 });
-
